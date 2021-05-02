367dc931f23bc143fe0e745640582aefefbb089c
Zobok Ivan <ivan.zrobok@hotmail.com>
2021-05-02 13:53:35 +0300

    first

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using WebAppMvc.Models;
+using System.Linq;
+
+namespace WebAppMvc.Controllers
+{
+    public class HomeController : Controller
+    {
+        //private readonly ILogger<HomeController> _logger;
+
+        public ViewResult Index()
+        {
+            var hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Goot Morning" : "Goot Afternoon";
+            return View("MyView");
+        }
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)//показує чи пройшли в моделі атрибути Required
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
+
+    }
+}
---
+++ Models/GuestResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebAppMvc.Models
+{
+    public class GuestResponse
+    {
+        [Required(ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+        [Required(ErrorMessage ="Please enter your name")]
+        [RegularExpression(pattern:".+\\@.+\\..+", ErrorMessage = "Please enter a valid email address")]
+        public string Email { get; set;    }
+        [Required(ErrorMessage = "Please enter you phone number")]
+        public string Phone { get; set; }
+        [Required(ErrorMessage ="Please specify whethe  you will attend")]
+        public bool? WillAttend { get; set;  }
+    }
+}
